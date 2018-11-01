371c84c476e4124dbe5c321c145ca25873e1d5ee
SimonTheFirst <kirillsimonov@yandex.ru>
2018-11-01 10:08:55 +0300

    removes db password

--- CourseWorkClient/src/courseWorkClient/common/DataBaseInterface.java
+++ CourseWorkClient/src/courseWorkClient/common/DataBaseInterface.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class DataBaseInterface {
 	private static final String url = "jdbc:mysql://localhost:3306/coursework?useSSL=false&serverTimezone=Europe/Moscow";
 	private static final String user = "root";
-	private static final String password = "2hj7x0rHitman";
+	private static final String password = "";//enter your password here
 	static Connection connection;
 	
 	public Connection connect()
